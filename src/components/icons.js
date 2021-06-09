23a4d2f7c33bb4681d552e7177ec70857319111d
KamranNmzv <kamrannamazov94@gmail.com>
2021-06-09 16:30:08 +0400

    first commit

---
+++ src/components/icons.js
@@ -0,0 +1,371 @@
+import React from 'react'
+import './icons.css'
+
+export default function Icons() {
+    return (
+        <div>
+            <main>
+        <div class="d-flex z-index">
+            <div>
+                <img src=" /assets/images/logo.png" alt='logo'/>
+            </div>
+            <div></div>
+        </div>
+        <section>
+           
+          
+            <div class="container">
+                <div class="bg-blue">
+                <h3 class="text-center mb-5 mt-15" style={{fontWeight:800}}>DIGITAL PANEL</h3>
+                </div>
+                <p class="mt-lg-5">INTERNAL TOOLS</p>
+                <hr/>
+                <div class="d-flex justify-content-between">
+                     
+                    <div class="logo" style={{visibility: "hidden"}}>
+                        <a target="_blank" rel="noreferrer" href="https://ap-view-prod.ibar.az/">
+                        <figure>
+                            <img src='/assets/images/ibar.png'
+                                alt='admin' />
+                        </figure>
+                        <figcaption>ADMIN PANEL</figcaption>
+                         </a>
+                    </div>
+                    <div class="logo">
+                        <a target="_blank" rel="noreferrer" href="https://ap-view-prod.ibar.az/">
+                        <figure>
+                            <img src='/assets/images/ibar.png'
+                                alt='elel' />
+                        </figure>
+                        <figcaption>ADMIN PANEL</figcaption>
+                         </a>
+                    </div>
+                    <div class="logo">
+                        <a target="_blank" rel="noreferrer" href="https://ap-view-prod.ibar.az/" >
+                        <figure>
+                         <img src="/assets/images/manage.jfif" alt="manage engine" />
+                        </figure>
+                        <figcaption>MANAGE ENGINE</figcaption>
+                         </a>
+                    </div>
+                    <div class="logo">
+                        <a target="_blank" rel="noreferrer" href="https://www.naumen.ru/">
+                        <figure>
+                            <img src='/assets/images/naumen.png'
+                                alt='naumen' />
+                        </figure>
+                        <figcaption>NAUMEN</figcaption>
+                         </a>
+                    </div>
+                    <div class="logo" style={{visibility: "hidden"}}>
+                        <a target="_blank" rel="noreferrer" href="https://ap-view-prod.ibar.az/">
+                        <figure>
+                            <img src='/assets/images/naumen.png'
+                            alt='naumen' />
+                        </figure>
+                        <figcaption>Naumen</figcaption>
+                         </a>
+                    </div>
+                  
+                    </div>
+                 
+                    <p class="mt-lg-5">TECHNICAL TOOLS</p>
+                    <hr/>
+                    <div class="d-flex justify-content-between">
+                        <div class="logo">
+                            <a target="_blank" rel="noreferrer" href="https://gitlab.ibar.az/users/sign_in">
+                            <figure>
+                                <img src='/assets/images/gitlab.png'
+                                    alt='gitlab' />
+                            </figure>
+                            <figcaption>GITLAB</figcaption>
+                             </a>
+                        </div>
+                        
+                     <div class="logo">
+                        <a target="_blank" rel="noreferrer" href="https://firebase.google.com/?gclid=Cj0KCQjwhr2FBhDbARIsACjwLo1jlECaPEfK1M1y_GT_PiXvB7VORm-Mxyi_tRdAoTNBnpdmLXQkGKgaApgNEALw_wcB&gclsrc=aw.ds">
+                        <figure>
+                            <img src='/assets/images/firebase.png'
+                                alt='firebase' />
+                        </figure>
+                        <figcaption>FIREBASE</figcaption>
+                         </a>
+                    </div>
+
+                    <div class="logo">
+                        <a target="_blank" rel="noreferrer" href="https://www.jenkins.io/">
+                        <figure>
+                            <img src='/assets/images/jen.png'
+                                alt='jenkins' />
+                        </figure>
+                        <figcaption>JENKINS</figcaption>
+                         </a>
+                    </div>
+    
+                        <div class="logo">
+                            <a target="_blank" rel="noreferrer" href="https://kibana-openshift-logging.apps.digital-prod1.ocp.ibar.az/">
+                            <figure>
+                                <img src='/assets/images/kibana.png'
+                                    alt='kibana' />
+                            </figure>
+                            <figcaption>KIBANA</figcaption>
+                             </a>
+                        </div>
+    
+                        <div class="logo">
+                            <a target="_blank" rel="noreferrer" href="https://xmi244.dynatrace-managed.com/e/10e800ec-dc4d-4b88-b964-48af15001560/login?backUri=%2Fui%2Fdashboards%3Fgtf%3D-2h%26gf%3Dall">
+                            <figure>
+                                <img src='/assets/images/dy.png'
+                                    alt='dynatrace' />
+                            </figure>
+                            <figcaption>DYNATRACE</figcaption>
+                             </a>
+                        </div>
+                    </div>
+
+                    <div class="d-flex justify-content-between">  
+                      
+                        <div class="logo">
+                            <a target="_blank" rel="noreferrer" href="https://oauth-openshift.apps.digital-prod1.ocp.ibar.az/">
+                            <figure>
+                                <img src='/assets/images/open.webp'
+                                    alt='openshift' />
+                            </figure>
+                            <figcaption>OPENSHIFT</figcaption>
+                             </a>
+                        </div>
+                        <div class="logo">
+                            <a target="_blank" rel="noreferrer" href="https://www.figma.com/">
+                            <figure>
+                                <img src='/assets/images/figma.webp'
+                                    alt='figma' />
+                            </figure>
+                            <figcaption>FIGMA</figcaption>
+                             </a>
+                        </div>
+                        
+                        
+    
+                        <div class="logo">
+                            <a target="_blank" rel="noreferrer" href="https://www.getxray.app/">
+                            <figure>
+                                <img src='/assets/images/xray.png'
+                                    alt='xray' 
+                                    style={{objectFit: 'contain'}}/>
+                            </figure>
+                            <figcaption>XRAY</figcaption>
+                             </a>
+                        </div>
+                        <div class="logo">
+                            <a target="_blank" rel="noreferrer" href="https://www.jetbrains.com/">
+                            <figure>
+                                <img src='/assets/images/jetbr.png'
+                                    alt='brains' />
+                            </figure>
+                            <figcaption>JETBRAINS</figcaption>
+                             </a>
+                        </div>
+                        <div class="logo">
+                            <a target="_blank" rel="noreferrer" href="https://www.jira.com/">
+                            <figure>
+                                <img src='https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_4102607129f4f7012d701f4b874bd142/scriptrunner-for-jira.png'
+                                    alt='script runner' />
+                            </figure>
+                            <figcaption>SCRIPT RUNNER</figcaption>
+                             </a>
+                        </div>
+                    </div>
+             
+                <p class="mt-lg-5">  BUSINESS TOOLS</p>
+                <hr/>
+              <div class="w-100">
+                <div class="d-flex justify-content-between">
+                    
+                   
+                    <div class="logo">
+                        <a target="_blank" rel="noreferrer" href="https://jira.com/">
+                        <figure>
+                            <img src='/assets/images/jira.png'
+                                alt='jira' />
+                        </figure>
+                        <figcaption>JIRA CLOUD</figcaption>
+                         </a>
+                    </div>
+
+                    <div class="logo">
+                        <a target="_blank" rel="noreferrer" href="https://iba-dit.atlassian.net/wiki/spaces/">
+                        <figure>
+                            <img src='/assets/images/conf.png'
+                                alt='confluence' />
+                        </figure>
+                        <figcaption>CONFLUENCE CLOUD</figcaption>
+                         </a>
+                    </div>
+                    <div class="logo">
+                        <a target="_blank" rel="noreferrer" href="https://capacitytracker.com/?ReturnUrl=%2Fhome">
+                        <figure>
+                            <img src='/assets/images/ct.jpg'
+                                alt='tracker' />
+                        </figure>
+                        <figcaption>CAPACITY TRACKER</figcaption>
+                         </a>
+                    </div>
+                    <div class="logo">
+                        <a target="_blank" rel="noreferrer" href="https://www.contentful.com/">
+                        <figure>
+                            <img src='/assets/images/contentful.png'
+                                alt='contentful' />
+                        </figure>
+                        <figcaption>CONTENTFUL</figcaption>
+                         </a>
+                    </div>
+
+                    <div class="logo">
+                        <a target="_blank" rel="noreferrer" href="https://miro.com/app/dashboard/">
+                        <figure>
+                            <img src='/assets/images/miro.png'
+                                alt='miro' />
+                        </figure>
+                        <figcaption>MIRO</figcaption>
+                         </a>
+                    </div>
+
+
+                   
+
+                   
+                </div>
+
+                <div class="d-flex justify-content-between">
+                    
+                   
+                    <div class="logo" style={{visibility: "hidden"}}>
+                        <a target="_blank" rel="noreferrer" href="https://dashboard.instabug.com/dashboard">
+                        <figure>
+                            <img src='/assets/images/insta.png'
+                                alt='instabug' />
+                        </figure>
+                        <figcaption>INSTABUG</figcaption>
+                         </a>
+                    </div>
+                    <div class="logo">
+                        <a target="_blank" rel="noreferrer" href="https://dashboard.instabug.com/dashboard">
+                        <figure>
+                            <img src='/assets/images/insta.png'
+                            alt='instabug' />
+                        </figure>
+                        <figcaption>INSTABUG</figcaption>
+                         </a>
+                    </div>
+
+
+                    <div class="logo">
+                        <a target="_blank" rel="noreferrer" href="https://app.screenful.me/auth/login?redirectUrl=https:~2F~2Fapp.screenful.me~2Fdashboard~2F79314~2Fmonitoring-resolution-time-kpi-s">
+                            <figure>
+                                <img src='/assets/images/screen.png'
+                                    alt='instabug' />
+                            </figure>
+                            <figcaption>SCREENFULL</figcaption>
+                             </a>
+                    </div>
+                    
+                    <div class="logo">
+                        <a target="_blank" rel="noreferrer" href="https://app.clickup.com/3766637/v/l/li/24896135?sh=sh">
+                        <figure>
+                            <img src='/assets/images/klik.png'
+                                alt='instabug' />
+                        </figure>
+                        <figcaption>CLICKUP</figcaption>
+                         </a>
+                    </div>
+                    <div class="logo" style={{visibility: "hidden"}}>
+                        <a target="_blank" rel="noreferrer" href="https://www.naumen.ru/">
+                        <figure>
+                            <img src='/assets/images/naumen.png'
+                                alt='naumen' />
+                        </figure>
+                        <figcaption>NAUMEN</figcaption>
+                         </a>
+                    </div>
+
+                   
+                </div>
+              </div>
+
+
+               
+                 <p class="mt-lg-5"> SOCIAL TOOLS</p>
+                 <hr/>
+                 <div class="d-flex justify-content-between">
+                     
+                    <div class="logo" style={{visibility: "hidden"}}>
+                        <a target="_blank" rel="noreferrer" href="https://slack.com/intl/en-az/">
+                        <figure>
+                            <img src='/assets/images/slack.png'
+                                alt='slack' />
+                        </figure>
+                        <figcaption>SLACK</figcaption>
+                         </a>
+                    </div>
+                    <div class="logo" style={{visibility: "hidden"}}>
+                         <a target="_blank" rel="noreferrer" href="https://ibar.zoom.us/">
+                         <figure>
+                             <img src='/assets/images/zoom.png'
+                                 alt='zoom' />
+                         </figure>
+                         <figcaption>ZOOM</figcaption>
+                          </a>
+                     </div>
+ 
+                     <div class="logo">
+                        <a target="_blank" rel="noreferrer" href="https://slack.com/intl/en-az/">
+                        <figure>
+                            <img src='/assets/images/slack.png'
+                                alt='slack' />
+                        </figure>
+                        <figcaption>SLACK</figcaption>
+                         </a>
+                    </div>
+                     <div class="logo">
+                         <a target="_blank" rel="noreferrer" href="https://ibar.zoom.us/">
+                         <figure>
+                            <img src='/assets/images/zoom.png'
+                                 alt='zoom' />
+                         </figure>
+                         <figcaption>ZOOM</figcaption>
+                          </a>
+                     </div>
+ 
+                     <div class="logo" style={{visibility: "hidden"}}>
+                        <a target="_blank" rel="noreferrer" href="https://slack.com/intl/en-az/">
+                        <figure>
+                            <img src='/assets/images/slack.png'
+                            alt='slack' />
+                        </figure>
+                        <figcaption>SLACK</figcaption>
+                         </a>
+                    </div>
+                     <div class="logo" style={{visibility: "hidden"}}>
+                         <a target="_blank" rel="noreferrer" href="https://ibar.zoom.us/">
+                         <figure>
+                            <img src='/assets/images/zoom.png'
+                                 alt='zoom' />
+                         </figure>
+                        
+                         <figcaption>ZOOM</figcaption>
+                          </a>
+                     </div>
+ 
+                    
+                 </div>
+               
+
+
+
+                
+            </div>
+        </section>
+    </main>
+        </div>
+    )
+}
